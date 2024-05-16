2af2d9ce62af7ad5e5a5dcf0d81c7bdd615b8275
Shems Petremand <shems.petremand.etu@univ-lille.fr>
2024-05-16 12:00:51 +0200

    pip-mpu : pip_cut_memory_block Impl

--- src/pip_mpu/mod.rs
+++ src/pip_mpu/mod.rs
@@ -14,3 +14,22 @@ pub fn pip_create_partition(block_local_id: &*const u32) -> u32 {
 
     r0
 }
+
+#[inline]
+pub fn pip_cut_memory_block(
+    block_to_cut_local_id: &*const u32,
+    cut_addr: &*const u32,
+    mpu_region_nb: i32,
+) -> *const u32 {
+    let r0: *const u32;
+    unsafe {
+        asm!(
+            "svc #1",
+            inout("r0") block_to_cut_local_id => r0,
+            in("r1") cut_addr,
+            in("r2") mpu_region_nb,
+        )
+    }
+
+    r0
+}
