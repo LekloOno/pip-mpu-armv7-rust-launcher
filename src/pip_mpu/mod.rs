3588bc795f29cac5a09428227f824284b23c2ecc
Shems Petremand <shems.petremand.etu@univ-lille.fr>
2024-05-16 11:42:13 +0200

    pip-mpu : pip_create_partition Impl

--- src/pip_mpu/mod.rs
+++ src/pip_mpu/mod.rs
@@ -1,1 +1,16 @@
 mod pip_items;
+use core::arch::asm;
+
+#[inline]
+pub fn pip_create_partition(mut block_local_id: *const u32) -> *const u32 {
+    let r0: u32;
+    unsafe {
+        //no memory clobber, it is default behavior in rust
+        asm!(
+            "svc #0",
+            inout("r0") block_local_id,
+        );
+    }
+
+    block_local_id
+}
