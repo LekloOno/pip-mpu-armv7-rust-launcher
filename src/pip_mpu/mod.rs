747630de91636d9c2d7d91071e87073fe5c124af
Shems Petremand <shems.petremand.etu@univ-lille.fr>
2024-05-16 11:53:49 +0200

    pip-mpu : pip_create_partition Fix - should return 0 or 1, not the address of the block

--- src/pip_mpu/mod.rs
+++ src/pip_mpu/mod.rs
@@ -2,15 +2,15 @@ mod pip_items;
 use core::arch::asm;
 
 #[inline]
-pub fn pip_create_partition(mut block_local_id: *const u32) -> *const u32 {
+pub fn pip_create_partition(block_local_id: &*const u32) -> u32 {
     let r0: u32;
     unsafe {
         //no memory clobber, it is default behavior in rust
         asm!(
             "svc #0",
-            inout("r0") block_local_id,
+            inout("r0") block_local_id => r0,
         );
     }
 
-    block_local_id
+    r0
 }
