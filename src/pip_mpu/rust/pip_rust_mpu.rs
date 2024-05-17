25c348cccc2eaeb680e9849f59d6a0a43ca89410
Shems Petremand <shems.petremand.etu@univ-lille.fr>
2024-05-17 10:16:20 +0200

    pip-rust-mpu : create_partition Comments

--- src/pip_mpu/rust/pip_rust_mpu.rs
+++ src/pip_mpu/rust/pip_rust_mpu.rs
@@ -1,5 +1,17 @@
 use crate::pip_mpu::core::pip_core_mpu;
 
+/// Brief. 
+///     Creates a new child
+/// 
+/// Description
+///     The [createPartition] system call creates a new child (sub-partition of the
+///     current partition), e.g. initializes the block <idBlock> as a PD block and
+///     sets the current partition as the parent partition.
+///
+/// *   `block_local_id` - The block to become the child partition descriptor
+///
+/// Returns
+///     true if the operation was successful, false otherwise.
 pub fn create_partition(block_local_id: &*const u32) -> bool {
     (pip_core_mpu::pip_create_partition(block_local_id) & 1) == 1
 }
