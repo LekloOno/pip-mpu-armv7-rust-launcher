1c6ea8b2a37a5e2ca5debf4277c077a76510d67e
Shems Petremand <shems.petremand.etu@univ-lille.fr>
2024-05-23 10:06:21 +0200

    pip-items : Frames getters shouldnt use mutable self reference

--- src/pip_mpu/core/pip_items.rs
+++ src/pip_mpu/core/pip_items.rs
@@ -51,23 +51,23 @@ pub struct BlockOrError {
 */
 // Frame trait allows for Generic Frame, replacing StackedFrame
 trait Frame {
-    fn sp(&mut self) -> u32;
-    fn r4(&mut self) -> u32;
-    fn r5(&mut self) -> u32;
-    fn r6(&mut self) -> u32;
-    fn r7(&mut self) -> u32;
-    fn r8(&mut self) -> u32;
-    fn r9(&mut self) -> u32;
-    fn r10(&mut self) -> u32;
-    fn r11(&mut self) -> u32;
-    fn r0(&mut self) -> u32;
-    fn r1(&mut self) -> u32;
-    fn r2(&mut self) -> u32;
-    fn r3(&mut self) -> u32;
-    fn r12(&mut self) -> u32;
-    fn lr(&mut self) -> u32;
-    fn pc(&mut self) -> u32;
-    fn xpsr(&mut self) -> u32;
+    fn sp(&self) -> u32;
+    fn r4(&self) -> u32;
+    fn r5(&self) -> u32;
+    fn r6(&self) -> u32;
+    fn r7(&self) -> u32;
+    fn r8(&self) -> u32;
+    fn r9(&self) -> u32;
+    fn r10(&self) -> u32;
+    fn r11(&self) -> u32;
+    fn r0(&self) -> u32;
+    fn r1(&self) -> u32;
+    fn r2(&self) -> u32;
+    fn r3(&self) -> u32;
+    fn r12(&self) -> u32;
+    fn lr(&self) -> u32;
+    fn pc(&self) -> u32;
+    fn xpsr(&self) -> u32;
 }
 
 pub struct BasicFrame {
@@ -75,55 +75,55 @@ pub struct BasicFrame {
 }
 
 impl Frame for BasicFrame {
-    fn sp(&mut self) -> u32 {
+    fn sp(&self) -> u32 {
         self.registers[0]
     }
-    fn r4(&mut self) -> u32 {
+    fn r4(&self) -> u32 {
         self.registers[1]
     }
-    fn r5(&mut self) -> u32 {
+    fn r5(&self) -> u32 {
         self.registers[2]
     }
-    fn r6(&mut self) -> u32 {
+    fn r6(&self) -> u32 {
         self.registers[3]
     }
-    fn r7(&mut self) -> u32 {
+    fn r7(&self) -> u32 {
         self.registers[4]
     }
-    fn r8(&mut self) -> u32 {
+    fn r8(&self) -> u32 {
         self.registers[5]
     }
-    fn r9(&mut self) -> u32 {
+    fn r9(&self) -> u32 {
         self.registers[6]
     }
-    fn r10(&mut self) -> u32 {
+    fn r10(&self) -> u32 {
         self.registers[7]
     }
-    fn r11(&mut self) -> u32 {
+    fn r11(&self) -> u32 {
         self.registers[8]
     }
-    fn r0(&mut self) -> u32 {
+    fn r0(&self) -> u32 {
         self.registers[9]
     }
-    fn r1(&mut self) -> u32 {
+    fn r1(&self) -> u32 {
         self.registers[10]
     }
-    fn r2(&mut self) -> u32 {
+    fn r2(&self) -> u32 {
         self.registers[11]
     }
-    fn r3(&mut self) -> u32 {
+    fn r3(&self) -> u32 {
         self.registers[12]
     }
-    fn r12(&mut self) -> u32 {
+    fn r12(&self) -> u32 {
         self.registers[13]
     }
-    fn lr(&mut self) -> u32 {
+    fn lr(&self) -> u32 {
         self.registers[14]
     }
-    fn pc(&mut self) -> u32 {
+    fn pc(&self) -> u32 {
         self.registers[15]
     }
-    fn xpsr(&mut self) -> u32 {
+    fn xpsr(&self) -> u32 {
         self.registers[16]
     }
 }
@@ -133,157 +133,157 @@ pub struct ExtendedFrame {
 }
 
 impl Frame for ExtendedFrame {
-    fn sp(&mut self) -> u32 {
+    fn sp(&self) -> u32 {
         self.registers[16]
     }
-    fn r4(&mut self) -> u32 {
+    fn r4(&self) -> u32 {
         self.registers[17]
     }
-    fn r5(&mut self) -> u32 {
+    fn r5(&self) -> u32 {
         self.registers[18]
     }
-    fn r6(&mut self) -> u32 {
+    fn r6(&self) -> u32 {
         self.registers[19]
     }
-    fn r7(&mut self) -> u32 {
+    fn r7(&self) -> u32 {
         self.registers[20]
     }
-    fn r8(&mut self) -> u32 {
+    fn r8(&self) -> u32 {
         self.registers[21]
     }
-    fn r9(&mut self) -> u32 {
+    fn r9(&self) -> u32 {
         self.registers[22]
     }
-    fn r10(&mut self) -> u32 {
+    fn r10(&self) -> u32 {
         self.registers[23]
     }
-    fn r11(&mut self) -> u32 {
+    fn r11(&self) -> u32 {
         self.registers[24]
     }
-    fn r0(&mut self) -> u32 {
+    fn r0(&self) -> u32 {
         self.registers[25]
     }
-    fn r1(&mut self) -> u32 {
+    fn r1(&self) -> u32 {
         self.registers[26]
     }
-    fn r2(&mut self) -> u32 {
+    fn r2(&self) -> u32 {
         self.registers[27]
     }
-    fn r3(&mut self) -> u32 {
+    fn r3(&self) -> u32 {
         self.registers[28]
     }
-    fn r12(&mut self) -> u32 {
+    fn r12(&self) -> u32 {
         self.registers[29]
     }
-    fn lr(&mut self) -> u32 {
+    fn lr(&self) -> u32 {
         self.registers[30]
     }
-    fn pc(&mut self) -> u32 {
+    fn pc(&self) -> u32 {
         self.registers[31]
     }
-    fn xpsr(&mut self) -> u32 {
+    fn xpsr(&self) -> u32 {
         self.registers[32]
     }
 }
 
 impl ExtendedFrame {
-    pub fn s16(&mut self) -> u32 {
+    pub fn s16(&self) -> u32 {
         self.registers[0]
     }
-    pub fn s17(&mut self) -> u32 {
+    pub fn s17(&self) -> u32 {
         self.registers[1]
     }
-    pub fn s18(&mut self) -> u32 {
+    pub fn s18(&self) -> u32 {
         self.registers[2]
     }
-    pub fn s19(&mut self) -> u32 {
+    pub fn s19(&self) -> u32 {
         self.registers[3]
     }
-    pub fn s20(&mut self) -> u32 {
+    pub fn s20(&self) -> u32 {
         self.registers[4]
     }
-    pub fn s21(&mut self) -> u32 {
+    pub fn s21(&self) -> u32 {
         self.registers[5]
     }
-    pub fn s22(&mut self) -> u32 {
+    pub fn s22(&self) -> u32 {
         self.registers[6]
     }
-    pub fn s23(&mut self) -> u32 {
+    pub fn s23(&self) -> u32 {
         self.registers[7]
     }
-    pub fn s24(&mut self) -> u32 {
+    pub fn s24(&self) -> u32 {
         self.registers[8]
     }
-    pub fn s25(&mut self) -> u32 {
+    pub fn s25(&self) -> u32 {
         self.registers[9]
     }
-    pub fn s26(&mut self) -> u32 {
+    pub fn s26(&self) -> u32 {
         self.registers[10]
     }
-    pub fn s27(&mut self) -> u32 {
+    pub fn s27(&self) -> u32 {
         self.registers[11]
     }
-    pub fn s28(&mut self) -> u32 {
+    pub fn s28(&self) -> u32 {
         self.registers[12]
     }
-    pub fn s29(&mut self) -> u32 {
+    pub fn s29(&self) -> u32 {
         self.registers[13]
     }
-    pub fn s30(&mut self) -> u32 {
+    pub fn s30(&self) -> u32 {
         self.registers[14]
     }
-    pub fn s31(&mut self) -> u32 {
+    pub fn s31(&self) -> u32 {
         self.registers[15]
     }
-    pub fn s0(&mut self) -> u32 {
+    pub fn s0(&self) -> u32 {
         self.registers[33]
     }
-    pub fn s1(&mut self) -> u32 {
+    pub fn s1(&self) -> u32 {
         self.registers[34]
     }
-    pub fn s2(&mut self) -> u32 {
+    pub fn s2(&self) -> u32 {
         self.registers[35]
     }
-    pub fn s3(&mut self) -> u32 {
+    pub fn s3(&self) -> u32 {
         self.registers[36]
     }
-    pub fn s4(&mut self) -> u32 {
+    pub fn s4(&self) -> u32 {
         self.registers[37]
     }
-    pub fn s5(&mut self) -> u32 {
+    pub fn s5(&self) -> u32 {
         self.registers[38]
     }
-    pub fn s6(&mut self) -> u32 {
+    pub fn s6(&self) -> u32 {
         self.registers[39]
     }
-    pub fn s7(&mut self) -> u32 {
+    pub fn s7(&self) -> u32 {
         self.registers[40]
     }
-    pub fn s8(&mut self) -> u32 {
+    pub fn s8(&self) -> u32 {
         self.registers[41]
     }
-    pub fn s9(&mut self) -> u32 {
+    pub fn s9(&self) -> u32 {
         self.registers[42]
     }
-    pub fn s10(&mut self) -> u32 {
+    pub fn s10(&self) -> u32 {
         self.registers[43]
     }
-    pub fn s11(&mut self) -> u32 {
+    pub fn s11(&self) -> u32 {
         self.registers[44]
     }
-    pub fn s12(&mut self) -> u32 {
+    pub fn s12(&self) -> u32 {
         self.registers[45]
     }
-    pub fn s13(&mut self) -> u32 {
+    pub fn s13(&self) -> u32 {
         self.registers[46]
     }
-    pub fn s14(&mut self) -> u32 {
+    pub fn s14(&self) -> u32 {
         self.registers[47]
     }
-    pub fn s15(&mut self) -> u32 {
+    pub fn s15(&self) -> u32 {
         self.registers[48]
     }
-    pub fn fpscr(&mut self) -> u32 {
+    pub fn fpscr(&self) -> u32 {
         self.registers[49]
     }
 }
