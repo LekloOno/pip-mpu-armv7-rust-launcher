624cd2ca7332a2ef4a18cebf7972725fd390f2ff
Shems Petremand <shems.petremand.etu@univ-lille.fr>
2024-05-27 16:57:33 +0200

    manage_partition : m_create_partition WIP

--- src/pip_mpu/manage_partition/manage_partition.rs
+++ src/pip_mpu/manage_partition/manage_partition.rs
@@ -1,11 +1,16 @@
+use crate::pip_mpu::core::pip_items::Interface;
 use crate::pip_mpu::manage_partition::partition_items::CreateReturn;
+use crate::pip_mpu::tools;
 
 pub fn m_create_partition(
+    interface: Interface,
     stack_size: usize,
     entry_point: *const u32,
     used_rom: usize,
-    unused_ram: usize,
-    unused_rom: usize,
+    unused_ram: usize, // 0 if leaf partition
+    unused_rom: usize, // 0 if leaf partition
 ) -> Result<CreateReturn, ()> {
+    let success_output = CreateReturn::new();
+    let pd_addr = tools::round((interface.ram_end as u32) - 543); //543 is 512 - 31, to make sure we do have 512 bits after align
     Err(())
 }
--- src/pip_mpu/manage_partition/partition_items.rs
+++ src/pip_mpu/manage_partition/partition_items.rs
@@ -1,9 +1,10 @@
 use crate::pip_mpu::core::pip_items::Interface;
+use core::ptr;
 
 pub struct Partition {
     pub stack_addr: *const u32,
     pub vidt_addr: *const u32,
-    pub itf: *const Interface,
+    pub interface_addr: *const Interface,
     pub stack_vidt_block_id: *const u32, //Block containing the stack & vidt
     pub interface_block_id: *const u32,  //Block containing the interface
     pub rom_block_id: *const u32,        //Block containing the used ROM
@@ -13,11 +14,37 @@ pub struct Partition {
     pub unused_rom_block_id: *const u32, //Block containing the unused ROM, NULL if this partition is a leaf partition
 
     //Merge data - used when deleting a partition to merge it back to its parent
-    pd_id: *const u32,
-    kern_id: *const u32,
+    pub pd_block_id: *const u32,
+    pub kern_block_id: *const u32,
+}
+
+impl Partition {
+    pub fn new() -> Self {
+        Partition {
+            stack_addr: ptr::null(),
+            vidt_addr: ptr::null(),
+            interface_addr: ptr::null(),
+            stack_vidt_block_id: ptr::null(),
+            interface_block_id: ptr::null(),
+            rom_block_id: ptr::null(),
+            unused_ram_block_id: ptr::null(),
+            unused_rom_block_id: ptr::null(),
+            pd_block_id: ptr::null(),
+            kern_block_id: ptr::null(),
+        }
+    }
 }
 
 pub struct CreateReturn {
-    pub partition: Partition,        //The created partition datas.
-    pub parent_kern_id: Option<u32>, //A new kernel structure, if it was required to create the requested partition
+    pub partition: Partition,            //The created partition datas.
+    pub parent_new_kern_id: Option<u32>, //A new kernel structure, if it was required to create the requested partition
+}
+
+impl CreateReturn {
+    pub fn new() -> Self {
+        Self {
+            partition: Partition::new(),
+            parent_new_kern_id: None,
+        }
+    }
 }
